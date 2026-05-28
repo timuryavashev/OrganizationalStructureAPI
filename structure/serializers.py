76a4ae1e176fe7547ff4193eed4a06ff6bc5a56e
timur <timuryavashev@gmail.com>
2026-05-28 15:46:32 +0400

    add children and employees in Department detail

--- structure/serializers.py
+++ structure/serializers.py
@@ -17,3 +17,54 @@ class EmployeeSerializer(serializers.ModelSerializer):
         model = Employee
         fields = '__all__'
         read_only_fields = ['department_id']
+
+
+class RecursiveDepartmentSerializer(serializers.ModelSerializer):
+
+    employees = serializers.SerializerMethodField()
+    children = serializers.SerializerMethodField()
+
+    class Meta:
+
+        model = Department
+        fields = [
+            'id',
+            'name',
+            'created_at',
+            'employees',
+            'children',
+        ]
+
+    def get_employees(self, obj):
+
+        include_employees = self.context.get('include_employees', True)
+
+        if not include_employees:
+            return []
+
+        employees = obj.employees.all().order_by('full_name')
+
+        return EmployeeSerializer(employees, many=True).data
+
+    def get_children(self, obj):
+
+        depth = self.context.get('depth', 1)
+
+        if depth <= 0:
+            return []
+
+        children = obj.children.all()
+
+        serializer = RecursiveDepartmentSerializer(
+            children,
+            many=True,
+            context={
+                'depth': depth - 1,
+                'include_employees': self.context.get(
+                    'include_employees',
+                    True
+                )
+            }
+        )
+
+        return serializer.data
--- structure/views.py
+++ structure/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, generics
 from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 from structure.models import Department
-from structure.serializers import DepartmentSerializer, EmployeeSerializer
+from structure.serializers import DepartmentSerializer, EmployeeSerializer, RecursiveDepartmentSerializer
 
 
 class DepartmentViewSet(viewsets.ModelViewSet):
@@ -10,6 +11,24 @@ class DepartmentViewSet(viewsets.ModelViewSet):
     serializer_class = DepartmentSerializer
     queryset = Department.objects.all()
 
+    def retrieve(self, request, *args, **kwargs):
+
+        department = self.get_object()
+
+        depth = int(request.query_params.get('depth', 1))
+        include_employees = (request.query_params.get('include_employees', 'true').lower() == 'true')
+
+        depth = min(depth, 5)
+
+        serializer = RecursiveDepartmentSerializer(
+            department,
+            context={
+                'depth': depth,
+                'include_employees': include_employees,
+            }
+        )
+
+        return Response(serializer.data)
 
 class EmployeeCreateAPIView(generics.CreateAPIView):
 
