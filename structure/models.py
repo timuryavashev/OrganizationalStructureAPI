0c07d25ebdbef4a2bd978a0e144fb15ad294b9ab
timur <timuryavashev@gmail.com>
2026-05-28 13:40:39 +0400

    add models and migrations

--- structure/models.py
+++ structure/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+
+    name = models.CharField(max_length=200, verbose_name='Название подразделения')
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Связанное подразделение', related_name='departments')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+
+        verbose_name = 'Подразделение'
+        verbose_name_plural = 'Подразделения'
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+
+    departament_id = models.ForeignKey(Department, on_delete=models.CASCADE, verbose_name='Подразделение', related_name='employers')
+    full_name = models.CharField(max_length=200, verbose_name='Полное имя')
+    position = models.CharField(max_length=200, verbose_name = 'Должность')
+    hired_at = models.DateField(null=True, blank=True, verbose_name='Дата трудоустройства')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return self.full_name
